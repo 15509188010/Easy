707cf21bfdc6374f5b8eb1a2d3ca7ef2559c4561
chengsimingLIVE@163.com <chengsimingLIVE@163.com>
2020-01-17 10:20:50 +0800

    init

---
+++ App/HttpController/Index.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\HttpController;
+
+use App\Model\User;
+use EasySwoole\Http\AbstractInterface\Controller;
+use EasySwoole\Component\Csp;
+use EasySwoole\ORM\DbManager;
+
+class Index extends Controller
+{
+    private $objMysqlPool;
+    private $objUser;
+
+    /**
+     * Index constructor.
+     * @throws \EasySwoole\ORM\Exception\Exception
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->objMysqlPool = DbManager::getInstance();
+        $this->objUser = new User();
+    }
+
+
+    /**
+     * @throws \EasySwoole\ORM\Exception\Exception
+     */
+    public function index()
+    {
+        var_dump($this->objUser->schemaInfo());
+
+        $all = [
+            [
+                'id'        => 1,
+                'payAmount' => 100,
+            ],
+            [
+                'id'        => 2,
+                'payAmount' => 200,
+            ]
+        ];
+        go(function () use ($all) {
+            $csp = new Csp();
+            $csp->add('t1', function () use ($all) {
+                \co::sleep(10);
+                return false;
+            });
+            $csp->add('t2', function () use ($all) {
+                \co::sleep(1);
+                return true;
+            });
+            var_dump($csp->exec());
+            var_dump($csp->successNum());
+        });
+
+    }
+
+    protected function actionNotFound(?string $action)
+    {
+        $this->response()->withStatus(404);
+        $file = EASYSWOOLE_ROOT . '/vendor/easyswoole/easyswoole/src/Resource/Http/404.html';
+        if (!is_file($file)) {
+            $file = EASYSWOOLE_ROOT . '/src/Resource/Http/404.html';
+        }
+        $this->response()->write(file_get_contents($file));
+    }
+}
---
+++ EasySwooleEvent.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace EasySwoole\EasySwoole;
+
+use EasySwoole\EasySwoole\Swoole\EventRegister;
+use EasySwoole\EasySwoole\AbstractInterface\Event;
+use EasySwoole\Http\Request;
+use EasySwoole\Http\Response;
+use EasySwoole\ORM\DbManager;
+use EasySwoole\ORM\Db\Connection;
+use EasySwoole\ORM\Db\Config;
+
+use App\Process\HotReload;
+
+class EasySwooleEvent implements Event
+{
+
+    public static function initialize()
+    {
+        // TODO: Implement initialize() method.
+        date_default_timezone_set('Asia/Shanghai');
+    }
+
+    /**
+     * @param EventRegister $register
+     * @throws \EasySwoole\Pool\Exception\Exception
+     */
+    public static function mainServerCreate(EventRegister $register)
+    {
+        // TODO: Implement mainServerCreate() method.
+        /************************************MYSQL连接池********************************************/
+        $mysqlConf = \EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL');
+        $config = new Config();
+        $config->setDatabase($mysqlConf['database']);
+        $config->setUser($mysqlConf['user']);
+        $config->setPassword($mysqlConf['password']);
+        $config->setHost($mysqlConf['host']);
+        //连接池配置
+        $config->setGetObjectTimeout(3.0); //设置获取连接池对象超时时间
+        $config->setIntervalCheckTime(30 * 1000); //设置检测连接存活执行回收和创建的周期
+        $config->setMaxIdleTime(15); //连接池对象最大闲置时间(秒)
+        $config->setMaxObjectNum(20); //设置最大连接池存在连接对象数量
+        $config->setMinObjectNum(5); //设置最小连接池存在连接对象数量
+        DbManager::getInstance()->addConnection(new Connection($config));
+
+        /***************************************注册热重启进程*****************************************/
+        $swooleServer = ServerManager::getInstance()->getSwooleServer();
+        $swooleServer->addProcess((new HotReload('HotReload', ['disableInotify' => true]))->getProcess());
+    }
+
+    public static function onRequest(Request $request, Response $response): bool
+    {
+        // TODO: Implement onRequest() method.
+        return true;
+    }
+
+    public static function afterRequest(Request $request, Response $response): void
+    {
+        // TODO: Implement afterAction() method.
+    }
+}
